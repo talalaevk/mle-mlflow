01c5f51e8e4cacdc07ad1329affd4a43b7b27af0
talalaevk <kirill.mipt2013@gmail.ccom>
2024-03-20 20:50:46 +0000

    logging

--- run_mlflow_server.sh
+++ run_mlflow_server.sh
@@ -1,4 +1,4 @@
 mlflow server \
 --backend-store-uri postgresql://$DB_DESTINATION_USER:$DB_DESTINATION_PASSWORD@$DB_DESTINATION_HOST:$DB_DESTINATION_PORT/$DB_DESTINATION_NAME \
---default-artifact-root s3://$AWS_BUCKET_NAME \
+--default-artifact-root s3://$S3_BUCKET_NAME \
 --no-serve-artifacts
