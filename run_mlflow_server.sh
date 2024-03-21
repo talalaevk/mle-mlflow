588c36e98e678828cd0204a3dd3399101edf0909
talalaevk <kirill.mipt2013@gmail.ccom>
2024-03-21 21:23:30 +0000

    model_logging

--- run_mlflow_server.sh
+++ run_mlflow_server.sh
@@ -1,4 +1,9 @@
+if [ -f .env ]; then
+    export $(cat .env | grep -v '#' | xargs)
+fi
+
 mlflow server \
 --backend-store-uri postgresql://$DB_DESTINATION_USER:$DB_DESTINATION_PASSWORD@$DB_DESTINATION_HOST:$DB_DESTINATION_PORT/$DB_DESTINATION_NAME \
---default-artifact-root s3://$S3_BUCKET_NAME \
+--registry-store-uri postgresql://$DB_DESTINATION_USER:$DB_DESTINATION_PASSWORD@$DB_DESTINATION_HOST:$DB_DESTINATION_PORT/$DB_DESTINATION_NAME \
+--default-artifact-root s3://$AWS_BUCKET_NAME \
 --no-serve-artifacts
